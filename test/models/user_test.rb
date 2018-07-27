9f184f25532d3ddb0c77b4bf1076f8519e2f56b8
RinaSuzuki <suzuki-s1-rina@jna.co.jp>
2018-07-27 10:29:13 +0900

    Finish user edit, update, index,and destroy actions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :bigint(8)        not null, primary key
+#  admin           :boolean          default(FALSE)
 #  email           :string
 #  name            :string
 #  password_digest :string
